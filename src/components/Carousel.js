9ef907b21c4557dfa6baf8baeb37b3428e2871c3
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 10:29:51 +0000

    Carousel.js: changed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,9 +6,7 @@ export default class ActionIcon extends React.Component {
     render() {
 
         return (
-            <Link href={withPrefix(url)} {...attrs} className={classes}>
-                <Icon icon={icon} />
-                <span className="sr-only">{label}</span>
+            ame="sr-only">{label}</span>
             </Link>
         );
     }
